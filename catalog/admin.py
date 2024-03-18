020287d3d73acbacd3efe0e484ab98dd71606645
Mahdi Fatemiy Kia <fatemikiamahdi67@gmail.com>
2024-03-18 03:47:27 +0330

    customizing admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,8 +2,42 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
