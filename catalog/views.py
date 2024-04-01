7b3aa83757ed53b663aad8e4e0877d1f6cf7e69f
Mahdi Fatemiy Kia <fatemikiamahdi67@gmail.com>
2024-04-02 03:03:16 +0330

    author and book (detail/listing) views

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import *
+from django.views import generic
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
@@ -22,3 +25,34 @@ def index(request):
 
     return render(request, 'index.html', context=context)
 
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'  # your own name for the list as a template variable
+    queryset = Book.objects.all()  # Get 5 books containing the title war
+    template_name = 'catalog/book_list.html'  # Specify your own template name/location
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def book_detail_view(request, primary_key):
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = 'author_list'  # your own name for the list as a template variable
+    queryset = Author.objects.all()  # Get 5 books containing the title war
+    template_name = 'catalog/author_list.html'  # Specify your own template name/location
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+    def author_detail_view(request, primary_key):
+        author = get_object_or_404(Author, pk=primary_key)
+        return render(request, 'catalog/author_detail.html', context={'author': author})
