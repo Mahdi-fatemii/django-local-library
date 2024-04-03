09e2f958369d2665e25c4d1a0994877f025aaac4
Mahdi Fatemiy Kia <fatemikiamahdi67@gmail.com>
2024-04-03 20:09:03 +0330

    users visiting count by django sessions

--- catalog/views.py
+++ catalog/views.py
@@ -15,7 +15,8 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
     num_authors = Author.objects.count()
-
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
